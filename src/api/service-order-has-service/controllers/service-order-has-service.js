6180df83872b9cca2283994754071596967ae342
Guilherme Bachega Gomes <guizobachegagomes@gmail.com>
2022-05-20 21:47:56 -0300

    'database_created'

---
+++ src/api/service-order-has-service/controllers/service-order-has-service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  service-order-has-service controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::service-order-has-service.service-order-has-service');
---
+++ src/api/service-order-has-service/services/service-order-has-service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-order-has-service service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::service-order-has-service.service-order-has-service');
