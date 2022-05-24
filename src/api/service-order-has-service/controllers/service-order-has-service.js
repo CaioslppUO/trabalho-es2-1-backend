97149fef71a27d5580b88dad5ffb8ed4f2e32a67
Guilherme Bachega Gomes <guizobachegagomes@gmail.com>
2022-05-23 22:39:46 -0300

    task terminada aparentemente

--- src/api/service-order-has-service/controllers/service-order-has-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  service-order-has-service controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::service-order-has-service.service-order-has-service');
--- src/api/service-order-has-service/services/service-order-has-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * service-order-has-service service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::service-order-has-service.service-order-has-service');
