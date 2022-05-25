1462bf67c7341f813c3b89fd56dcaaeb9d90cf12
lucasgaravaglia <lucsagrafimar@gmail.com>
2022-05-24 22:25:56 -0300

    change in the service-order route in the findone function

--- src/api/service-order/controllers/service-order.js
+++ src/api/service-order/controllers/service-order.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  *  service-order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::service-order.service-order');
+module.exports = createCoreController(
+  "api::service-order.service-order",
+  () => ({
+    async findOne(args) {
+      args.query = { ...args.query, local: "en" };
+      const { data, meta } = await super.findOne(args);
+      let total = 0;
+      data.attributes.services.data.forEach((result) => {
+        total = total + result.attributes.price;
+      });
+
+      return { data, TotalOrderPrice: total };
+    },
+  })
+);
