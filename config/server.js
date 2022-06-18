89f5d45b17254bf72f7bf55aba1e773ae5553b5f
CaioslppUO <caioslppuo@gmail.com>
2022-06-17 21:26:48 -0300

    Removed old files

--- config/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-  url: env("https://sistema-nota-fiscal-test.herokuapp.com/"),
-  proxy: true,
-  app: {
-      keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
-  },
-});
