cf64e84c717decb4189fc6338a3246b2f6d9c6db
CaioslppUO <caioslppuo@gmail.com>
2022-06-18 00:02:50 -0300

    Added option to wipe the database

--- index.ts
+++ index.ts
@@ -4,17 +4,19 @@ import { exit } from "process";
 import { Phone } from "./src/api/phone/phone";
 
 // Initialize the database and populate it.
-const initialize_db = (): Promise<void> => {
+const initialize_db = (wipe_db: boolean): Promise<void> => {
   return new Promise(async (resolve, rejects) => {
     if (!fs.existsSync(__dirname + "/src/database/data.sqlite3")) {
       await database.migrate.latest();
+    }
+    if (wipe_db) {
       await database.seed.run();
     }
     resolve();
   });
 };
 
-initialize_db().then(() => {
+initialize_db(true).then(() => {
   let p = Phone();
   p.find().then((res) => {
     console.log(res);
