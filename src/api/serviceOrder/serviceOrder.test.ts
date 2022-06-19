1b93512930b3ae7bab8b2b1d62c661e7458dc683
CaioslppUO <caioslppuo@gmail.com>
2022-06-18 23:37:04 -0300

    Updated serviceOrder tests

--- src/api/serviceOrder/serviceOrder.test.ts
+++ src/api/serviceOrder/serviceOrder.test.ts
@@ -8,42 +8,56 @@ describe("Test the service order database operations", () => {
 
   let serviceOrder = ServiceOrder();
 
+  test("Should insert a service order", async () => {
+    await database("ServiceOrder").truncate();
+    await database.seed.run();
+    let res = await serviceOrder.insert(3, 5).then((res) => {
+      return serviceOrder
+        .findOne(res.id)
+        .then((res2) => {
+          res2[0].id = res.id;
+          return res2[0];
+        })
+        .catch((err) => err);
+    });
+    expect(res).toEqual({
+      cpf: "12345678912",
+      email: "arcanjolevi@gmail.com",
+      name: "Leví Cícero Arcanjo",
+      id: res.id,
+      idPhone: 5,
+      phoneModel: "Huawei",
+      services: [],
+    });
+  });
+
   test("Should not allow to insert a serviceOrder with a phone that doesn't exist", async () => {
     await database("ServiceOrder").truncate();
     await database.seed.run();
-    serviceOrder
+    let res = await serviceOrder
       .insert(1, 10)
-      .then((res) => {
-        expect(res.id).toBe(-1);
-      })
-      .catch((err) => {
-        expect(err).toBe("phone doesn't exist");
-      });
+      .then(() => {})
+      .catch((err) => err);
+    expect(res).toBe("phone doesn't exist");
   });
 
   test("Should not allow to insert a serviceOrder with a client that doesn't exist", async () => {
     await database("ServiceOrder").truncate();
     await database.seed.run();
-    serviceOrder
+    let res = await serviceOrder
       .insert(10, 1)
-      .then((res) => {
-        expect(res.id).toBe(-1);
-      })
-      .catch((err) => {
-        expect(err).toBe("client doesn't exist");
-      });
+      .then((res) => {})
+      .catch((err) => err);
+    expect(res).toBe("client doesn't exist");
   });
 
   test("Should not allow insert serviceOrder with client and phone that doesn't exist", async () => {
     await database("ServiceOrder").truncate();
     await database.seed.run();
-    serviceOrder
+    let res = await serviceOrder
       .insert(10, 10)
-      .then((res) => {
-        expect(res.id).toBe(-1);
-      })
-      .catch((err) => {
-        expect(err).toBe("phone doesn't exist");
-      });
+      .then((res) => {})
+      .catch((err) => err);
+    expect(res).toBe("phone doesn't exist");
   });
 });
