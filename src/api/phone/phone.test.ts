175be4e5505374a2218a8907ddcf49664c2d55e1
CaioslppUO <caioslppuo@gmail.com>
2022-06-18 23:03:26 -0300

    Updated phone tests

--- src/api/phone/phone.test.ts
+++ src/api/phone/phone.test.ts
@@ -3,11 +3,88 @@ import { database } from "../knex/knex";
 
 describe("Test the phone database operations", () => {
   afterAll(async () => {
-    await database("Client").truncate();
+    await database("Phone").truncate();
     await database.seed.run();
   });
 
   let phone = Phone();
+
+  test("Should insert a phone", async () => {
+    await database("Phone").truncate();
+    await database.seed.run();
+    let res = await phone
+      .insert("Novo Modelo")
+      .then((res) => {
+        return phone
+          .findOne(res.id)
+          .then((res2) => {
+            res2.id = res.id;
+            return res2[0];
+          })
+          .catch((err) => err);
+      })
+      .catch((err) => err);
+    expect(res).toEqual({ id: res.id, model: "Novo Modelo" });
+  });
+
+  test("Should delete a phone", async () => {
+    await database("Phone").truncate();
+    await database.seed.run();
+    let res = await phone
+      .remove(2)
+      .then(() => {
+        return phone
+          .findOne(2)
+          .then((res2) => {
+            return res2;
+          })
+          .catch((err) => err);
+      })
+      .catch((err) => err);
+    expect(res).not.toBe("undefined");
+    expect(res.length).toBe(0);
+  });
+
+  test("Should select a phone", async () => {
+    await database("Phone").truncate();
+    await database.seed.run();
+    let res = await phone
+      .findOne(3)
+      .then((res) => {
+        return res[0];
+      })
+      .catch((err) => err);
+    expect(res).toEqual({ id: 3, model: "Samsung" });
+  });
+
+  test("Should select all phones", async () => {
+    await database("Phone").truncate();
+    await database.seed.run();
+    let res = await phone
+      .find()
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => err);
+    expect(res).not.toBe("undefined");
+    expect(res.length).not.toBe("undefined");
+    expect(res.length).toBe(7);
+  });
+
+  test("Should update a phone", async () => {
+    await database("Phone").truncate();
+    await database.seed.run();
+    let res = await phone
+      .update(1, "Novo Modelo")
+      .then(() => {
+        return phone
+          .findOne(1)
+          .then((res) => res[0])
+          .catch((err) => err);
+      })
+      .catch((err) => err);
+    expect(res).toEqual({ id: 1, model: "Novo Modelo" });
+  });
 
   test("Should not insert a duplicated model", async () => {
     await database("Phone").truncate();
